dba1a128a7b3eaa3861fd2e51c205117be5be7f1
Alysson Deligny <alysson.deligny@gmail.com>
2024-11-08 16:01:45 +0100

    fix method deploy

--- core/src/main/java/io/github/civilisation/Civilisation.java
+++ core/src/main/java/io/github/civilisation/Civilisation.java
@@ -35,11 +35,13 @@ public class Civilisation extends Game{
     }
 
     public void deployUnits(Class<? extends Unit> unitClass) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        Unit unit = unitClass.getDeclaredConstructor().newInstance();
+
         int cost = unit.getCost();
         if (gold >= cost) {
+            Unit unit = unitClass.getDeclaredConstructor().newInstance();
             units.add(unit);
             gold -= cost;
+        }
     }
 
     public void useUniqueAbility(List<Unit> enemyUnits) {
