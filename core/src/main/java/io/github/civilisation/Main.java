caf77b0fcc07be52c47b96eec04e2ef664ac4a85
Ozheim <heikel.zouaoui@epitech.eu>
2024-11-08 10:00:26 +0100

    add create

--- core/src/main/java/io/github/civilisation/Main.java
+++ core/src/main/java/io/github/civilisation/Main.java
@@ -4,31 +4,49 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture image;
+    private Texture knightTexture;
+    private Animation<TextureRegion> walkAnimation;
+    private float elapsedTime;
+    private float knightX;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        knightTexture = new Texture("pictures/Knight/Knight_1/Walk.png");
+        TextureRegion[][] tmpFrames = TextureRegion.split(knightTexture, 73, knightTexture.getHeight());
+        TextureRegion[] walkFrames = new TextureRegion[5];
+        for (int i = 0; i < 5; i++) {
+            walkFrames[i] = tmpFrames[0][i];
+        }
+        walkAnimation = new Animation<>(0.1f, walkFrames);
+        walkAnimation.setPlayMode(Animation.PlayMode.LOOP);
+        elapsedTime = 0f;
+        knightX = 140;
     }
 
     @Override
     public void render() {
+        elapsedTime += Gdx.graphics.getDeltaTime();
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
+
+        TextureRegion currentFrame = walkAnimation.getKeyFrame(elapsedTime, true);
+        knightX += 100 * Gdx.graphics.getDeltaTime();
+
+        batch.draw(currentFrame, knightX, 210);
         batch.end();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        knightTexture.dispose();
     }
 }
